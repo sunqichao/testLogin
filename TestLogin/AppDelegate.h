acfe93be7ccbbb175c804bcad91558351816afed
sunqichao <sunqichaojob@gmail.com>
2014-01-13 22:06:01 +0800

    Initial Commit

---
+++ TestLogin/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TestLogin
+//
+//  Created by sunqichao on 14-1-13.
+//  Copyright (c) 2014年 sunqichao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TestLogin/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TestLogin
+//
+//  Created by sunqichao on 14-1-13.
+//  Copyright (c) 2014年 sunqichao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
